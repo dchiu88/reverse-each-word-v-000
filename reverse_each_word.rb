13bcb38f0d130de78f0f8f0fb4eb122b4ee2ad2c
David Chiu <davidwchiu@gmail.com>
2018-03-24 22:22:59 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,3 +4,10 @@ def reverse_each_word(string)
   end
   result.join(" ")
 end
+
+def reverse_each_word(string)
+  result = string.split().collect do |word|
+    word.reverse
+  end
+  result.join(" ")
+end
